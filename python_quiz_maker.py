2a304fa1b7151b4f4438f79def1ef8a48577d079
Ren <renzapnta4@gmail.com>
2025-04-14 14:56:13 +0800

    Added a block of code that, locates the file where the data will be saved at. It uses the open() function to do this, and the write() function to append the user input to the given text file.

--- python_quiz_maker.py
+++ python_quiz_maker.py
@@ -1,3 +1,5 @@
+#Asks the users to enter a question and four corresponding choices for it
+
 while True:
     user_question = input("Enter a question: ") #Asks the user for a question
 
@@ -7,7 +9,9 @@ while True:
         choice = input(f"Enter choice #{i + 1}: ")
         choices.append(choice) #Appends the choice inputs to the CHOICES list
 
-    while True: #A loop for determining the correct answer for the question input
+#A loop for determining the correct answer for the question input
+
+    while True: 
         try: 
             correct_choice = int(input(f"Which one is the correct answer? (1-4): ")) #Asks the user which of the choice input is the correct answer for the corresponding question
             if 1 <= correct_choice <= 4: #Verifies if the correct_choice input is within the numerical range of 1 and 4
@@ -17,5 +21,20 @@ while True:
 
         except ValueError:
             print("Please enter a valid input") #Prints if the input is non-numeric; invalid
+
+#This part is responsible for writing data onto the text file
+
+    with open(r"C:\Users\renza\OneDrive\Documents\School\College\2nd Semester\Object Oriented Programming\Programs\python_quiz_maker\quiz.txt", "a") as file: #This opens the file wherein the data 
+        file.write(f"Question: {user_question} \n") #This writes the actual question onto the text file
+        for i, choice in enumerate(choices, start=1): #Loops over each of the 4 choice inputs of the user
+            file.write(f"{i}. {choice}\n") #This writes the choices of a given question on the text file with the assigned number up front
+        file.write(f"Correct answer: {correct_choice} \n") #This writes the correct answer for the given question
+        
+
+    
+            
+
+
+
 
     
