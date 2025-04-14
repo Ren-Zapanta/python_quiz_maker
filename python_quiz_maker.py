961c226a32e4425cf3968a5e44c450722fb4dc4e
Ren <renzapnta4@gmail.com>
2025-04-14 12:12:30 +0800

    Creates a loop that assigns which choice in the list is the correct one.  The loop verifies if the entered input is a valid character (ranges from 1 to 4). If not, it asks the user to enter a valid enter.

--- python_quiz_maker.py
+++ python_quiz_maker.py
@@ -7,4 +7,15 @@ while True:
         choice = input(f"Enter choice #{i + 1}: ")
         choices.append(choice) #Appends the choice inputs to the CHOICES list
 
+    while True: #A loop for determining the correct answer for the question input
+        try: 
+            correct_choice = int(input(f"Which one is the correct answer? (1-4): ")) #Asks the user which of the choice input is the correct answer for the corresponding question
+            if 1 <= correct_choice <= 4: #Verifies if the correct_choice input is within the numerical range of 1 and 4
+                break #breaks loop if the input is valid 
+            else:
+                print("Please enter a number between 1 to 4") #Prints if the input is not within range
+
+        except ValueError:
+            print("Please enter a valid input") #Prints if the input is non-numeric; invalid
+
     
