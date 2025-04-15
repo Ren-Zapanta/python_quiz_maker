cb4b2d49873dded43b927ac0c536e475bf332fa3
Ren <renzapnta4@gmail.com>
2025-04-15 10:54:18 +0800

    Added a block of code that creates a blinking rectangle to imitate the blinking cursor right efter the last character typed.

--- hacker_terminal.py
+++ hacker_terminal.py
@@ -17,6 +17,8 @@ pygame.display.set_caption("Hacker Terminal") #Name of the window
 
 run = True #control variable that keeps the window running
 
+clock = pygame.time.Clock() #keeps track of time in-between frames
+
 while run:
 
     for event in pygame.event.get(): #Check for events like key presses or window closing
@@ -36,7 +38,15 @@ while run:
     text_surface = base_font.render(user_text, True, (0, 255, 0))
     screen.blit(text_surface, (100, 300))
 
+    time_now = pygame.time.get_ticks() #Fetches the current time since the program was started (in milliseconds)
+
+    if (time_now // 500) % 2 == 0:
+        text_width = text_surface.get_width() #Determines the width of the text
+        cursor_x = 100 + text_width #Places the blinking cursor right after the last character
+        cursor_y = 300 #Places the blinking cursor in the same y axis coordinate as the text
+        pygame.draw.rect(screen, (0, 255, 0), (cursor_x, cursor_y, 3.5, base_font.get_height())) #Draws the blinking object; contains the necessary dimensions and coordinates
 
     pygame.display.update() #Responsible for updating the screen for all changes
+    clock.tick(60) #Caps the program's framerate to 60 fps
 
 pygame.quit() #Close the Pygame window after loop ends
