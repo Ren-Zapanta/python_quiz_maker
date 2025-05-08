a82827381e40faebddedc504300d57be23fbf0ed
Ren <renzapnta4@gmail.com>
2025-05-09 01:18:44 +0800

    initialized the font that will beused throughout the program along with its color and the terminal's background fill color

--- quiz_program.py
+++ quiz_program.py
@@ -2,9 +2,14 @@ import pygame
 import sys
 import time
 
-pygame.init()#Initializes Pygame modules
+pygame.init() #Initializes Pygame modules
+
+base_font = pygame.font.SysFont("consolas", 27) #THe font that the program will be using, along with its size.
 
 #Sets the window's dimensions
 screen_width = 1200
 screen_height = 720
+pygame.display.set_caption("Hacker Terminal") #name of the window
 
+green = (0, 255, 0)  #color for the green text
+black = (0, 0, 0) #terminal's background color
